d257f2b83adbf635b4e4e0877de8afc684618947
Vincent Udén <vincentuden@gmail.com>
2025-06-14 13:27:16 +0200

    Move the test file

--- test/logPrice.test.ts
+++ test/logPrice.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { scrapePrice } from "./logPrice";
+import { scrapePrice } from "../src/pages/api/logPrice";
 
 test("Scraping works", async () => {
   let data = await scrapePrice();
