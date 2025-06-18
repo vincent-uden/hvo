70be07e6baf1a8cb38b9a9a265b5f7602887eb32
Vincent Udén <vincentuden@gmail.com>
2025-06-18 14:57:06 +0200

    Update cronjob to also use the new scraper

--- src/pages/api/today.ts
+++ src/pages/api/today.ts
@@ -3,40 +3,20 @@ import type { APIRoute } from "astro";
 import { parse } from "node-html-parser";
 import { db } from "../../db/drizzle";
 import { priceLogs, type NewPriceLog } from "../../db/schema";
+import { scrapePrice } from "./logPrice";
 
 export const GET: APIRoute = async ({ params, request }) => {
-  const response = await fetch(
-    "https://www.preem.se/foretag/kund-hos-preem/listpriser/listpriser-foretagskort/",
-  );
-  if (response.ok) {
-    let parsedHtml = parse(await response.text());
-
-    let priceTable = parsedHtml.querySelector("table");
-    let hvoRow = priceTable?.querySelectorAll("tr")[3];
-    let priceCell = hvoRow?.querySelectorAll("td")[1];
-    let dateCell = hvoRow?.querySelectorAll("td")[2];
-
-    let price = priceCell?.innerText
-      .replaceAll("\n", "")
-      .split(" ")[0]
-      .replace(",", ".");
-
-    if (price != null) {
-      let newPrice: NewPriceLog = {
-        price: price,
-      };
-
-      await db.insert(priceLogs).values(newPrice);
-    }
+  let data = await scrapePrice();
+  if (data !== undefined) {
+    await db.insert(priceLogs).values(data.log);
 
     return new Response(
       JSON.stringify({
-        price: Number(price),
-        date: dateCell?.innerText.replaceAll("\n", ""),
+        price: Number(data?.log.price),
+        date: data?.date,
       }),
     );
   }
-
   return new Response(
     JSON.stringify({
       message: "Preem couldn't be reached",
